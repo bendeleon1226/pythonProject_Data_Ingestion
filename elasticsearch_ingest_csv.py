a89266ef4aea549af48b41469ca444132235188e
Ben De Leon <bendeleonjr@hotmail.com>
2021-08-29 21:54:56 +0800

    ingest csv into elasticsearch 7.14.0

---
+++ elasticsearch_ingest_csv.py
@@ -0,0 +1,10 @@
+from elasticsearch import Elasticsearch, helpers
+import csv
+
+# Create the elasticsearch client.
+es = Elasticsearch(host = "localhost", port = 9200)
+
+# Open csv file and bulk upload
+with open('/home/se/Downloads/stockerbot-export.csv') as f:
+    reader = csv.DictReader(f)
+    helpers.bulk(es, reader, index='tweets')
